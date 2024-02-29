1c40fe929d125906124af46033fe5a2ad958e8fb
artalita <161663287+artalita@users.noreply.github.com>
2024-02-29 09:07:24 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1709172196,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/backgroundnight-sheet0.png",
+		"images/bluebirddownflap-sheet0.png",
+		"images/pipegreen-sheet0.png",
+		"images/pipegreen2-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
